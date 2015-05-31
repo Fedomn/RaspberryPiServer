9f63a922fa2a0bb4f07bbb7b4d5c0d62542f8d01
Fedomn <shadowforfiend@gmail.com>
2015-05-31 21:35:21 +0800

    Added class ResponseMaker

---
+++ src/main/java/com/fedomn/ResponseMaker.java
@@ -0,0 +1,11 @@
+package com.fedomn;
+
+/**
+ * Created by fedomn on 2015/5/31.
+ */
+public class ResponseMaker {
+
+    public static String translate(String request) {
+        return null;
+    }
+}

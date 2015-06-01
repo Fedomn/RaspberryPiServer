f2f612bd5d08fcd7c682cbb05200282289a97fca
Fedomn <shadowforfiend@gmail.com>
2015-06-01 14:03:59 +0800

    Wrote expected client code for using chain of responsibility pattern

--- src/main/java/com/fedomn/ResponseMaker.java
+++ src/main/java/com/fedomn/ResponseMaker.java
@@ -6,9 +6,17 @@ package com.fedomn;
 public class ResponseMaker {
 
     public static String translate(String request) {
+        RequestHandler commonRequestHandler = new CommonRequestHandler(null);
+        RequestHandler officeHandler = new OfficeHandler(commonRequestHandler);
+        RequestHandler officeFirstHandler = new OfficeFirstHandler(officeHandler);
+        RequestHandler playHandler = new PlayHandler(officeFirstHandler);
+        RequestHandler playFirstHandler = new PlayFirstHandler(playHandler);
+        return playFirstHandler.handler(request);
+
+
         if (request.equals("playFirst")) {
             return "/home/pi/server/omx_control.sh play";
-        }else if (request.equals("play")) {
+        } else if (request.equals("play")) {
             return "/home/pi/server/omx_control.sh";
         } else if (request.equals("officeFirst")) {
             return "libreoffice";
